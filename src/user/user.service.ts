324cecbd08a24d6a6b390bb69cebc8087b40b625
rubenkristian96 <rubenkristian96@gmail.com>
2024-11-01 21:21:59 +0700

    add docker compose to run mariadb and redis and update readme

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { Brackets, Repository } from 'typeorm';
 import * as moment from 'moment-timezone';
 import { MESSAGE_STATUS } from '../utils/constants';
 
@@ -60,13 +60,22 @@ export class UserService {
 
   async getTodayUserBirthday(): Promise<User[]> {
     const today = new Date();
-    const month = today.getMonth() + 1; // getMonth() returns 0-11
+    const month = today.getMonth() + 1;
     const date = today.getDate();
+    const oneYearAgo = new Date();
+    oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
 
     return await this.userRepository
       .createQueryBuilder('birthday')
       .where('EXTRACT(MONTH FROM birthday.birthday) = :month', {month: month})
       .andWhere('EXTRACT(DAY FROM birthday.birthday) = :date', {date: date})
+      .andWhere('last_status != :status', {status: MESSAGE_STATUS.FAILED})
+      .andWhere(
+        new Brackets(qb => {
+          qb.where('last_updated IS NULL')
+          .orWhere('last_updated < :year', {year: oneYearAgo})
+        })
+      )
       .getMany();
   }
 
